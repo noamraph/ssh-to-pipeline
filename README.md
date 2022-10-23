b00ba0ed3b60858a1433cf92cc8d758615b3df44
Noam Yorav-Raphael <noamraph@gmail.com>
2022-10-23 13:31:27 +0300

    Seems to work.

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
-# ssh-to-pipeline
-SSH to bitbucket pipeline
+# ssh-to-pipeline: SSH to bitbucket pipelines
+
+Sometimes the pipeline fails, and it would really help, in order to fix the
+problem, to SSH into the container running the script and try to find out
+why it doesn't work. This script helps you do that.
+
---
+++ ssh_to_pipeline.py
@@ -0,0 +1,120 @@
+#!/usr/bin/env python3
+# pylint: disable=unspecified-encoding disable=C
+
+from __future__ import annotations
+
+import json
+import os
+import re
+from pathlib import Path
+from subprocess import PIPE, Popen, check_call, check_output
+
+
+def update_authorized_keys() -> None:
+    authorized_keys = Path("~/.ssh/authorized_keys").expanduser()
+    ssh_pubkey = os.environ.get("SSH_PUBKEY")
+    if ssh_pubkey is not None:
+        authorized_keys.parent.mkdir(exist_ok=True)
+        with open(authorized_keys, "a") as f:
+            f.write("\n" + ssh_pubkey + "\n")
+        authorized_keys.chmod(0o644)
+    if not authorized_keys.exists():
+        raise RuntimeError(
+            "SSH_PUBKEY is not defined and ~/.ssh/authorized_keys file doesn't exist. "
+            "You won't be able to SSH into the container."
+        )
+
+
+def install_packages() -> None:
+    # Based on linux installation instructions at https://ngrok.com/docs/getting-started
+    ngrok_asc = check_output(
+        ["curl", "-s", "https://ngrok-agent.s3.amazonaws.com/ngrok.asc"],
+        encoding="ascii",
+    )
+    Path("/etc/apt/trusted.gpg.d/ngrok.asc").write_text(ngrok_asc)
+    Path("/etc/apt/sources.list.d/ngrok.list").write_text(
+        "deb https://ngrok-agent.s3.amazonaws.com buster main\n"
+    )
+    check_call(["apt-get", "update"])
+    check_call(
+        [
+            "apt-get",
+            "install",
+            "-y",
+            "--no-install-recommends",
+            "ngrok",
+            "openssh-server",
+        ]
+    )
+
+
+def fix_bitbucket_tty() -> None:
+    with Path("~/.bashrc").expanduser().open("a") as f:
+        f.write(
+            "\n"
+            "# Fix TTY set by bitbucket\n"
+            'if [[ "$SSH_TTY" == "$(readlink -f /dev/stdout)" ]]; then exec 1<&0 2<&0; fi\n'
+            "\n"
+        )
+
+
+def start_ssh_server(ngrok_token: str) -> None:
+    check_call(["ngrok", "config", "add-authtoken", ngrok_token])
+    Path("/run/sshd").mkdir(parents=True, exist_ok=True)
+    with Popen(["/usr/sbin/sshd", "-D"]) as sshd:
+        with Popen(
+            "ngrok tcp 22 --log=stdout --log-format=json | tee /dev/stderr",
+            shell=True,
+            stdout=PIPE,
+            bufsize=0,
+            encoding="ascii",
+        ) as ngrok:
+            assert ngrok.stdout is not None  # for mypy
+            while True:
+                line = ngrok.stdout.readline()
+                if line == "":
+                    break
+                d = json.loads(line)
+                if d.get("msg") == "started tunnel":
+                    url = d["url"]
+                    m = re.match(r"^tcp://(.+):(\d+)$", url)
+                    assert m is not None
+                    host, port = m.groups()
+                    print(f"\n\nTo connect, run:\n\nssh -p {port} root@{host}\n\n\n")
+        # If ngrok exited, close sshd
+        sshd.kill()
+
+
+def ssh_to_pipeline() -> None:
+    ngrok_token = os.environ.get("NGROK_TOKEN")
+    if ngrok_token is None:
+        raise RuntimeError(
+            "NGROK_TOKEN is not defined. Please define this environment variable."
+        )
+    if len(ngrok_token.strip().split()) != 1:
+        raise RuntimeError(
+            "NGROK_TOKEN is not one word. Please make sure it is just the token."
+        )
+
+    update_authorized_keys()
+
+    install_packages()
+
+    fix_bitbucket_tty()
+
+    start_ssh_server(ngrok_token)
+
+
+def main() -> None:
+    from argparse import ArgumentParser
+
+    parser = ArgumentParser(
+        description="Open a SSH server to allow access to the pipeline container"
+    )
+    _args = parser.parse_args()
+
+    ssh_to_pipeline()
+
+
+if __name__ == "__main__":
+    main()
